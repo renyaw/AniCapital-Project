64917ec74e2394ea13ad0e3a05786099d62839a2
renyaw <norarenada@students.undip.ac.id>
2024-06-04 03:13:14 +0700

    checkpoin

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const UserRoutes = require('./routes/UserRoute.js');
 const mongoose = require('mongoose');
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import UserRoute from "./routes/UserRoute.js";
 
 // Express App\
 
@@ -22,11 +19,10 @@ app.use('/api/Users',UserRoutes);
 
 // connect to db
 mongoose.connect(process.env.MONGO_DB);
+
 const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () =>console.log('Database Connected...'));
-
-
 
 
 // listen request
