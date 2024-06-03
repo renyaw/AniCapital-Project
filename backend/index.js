fc91ddb37a6cefad40c94be977443256d1c51a25
renyaw <norarenada@students.undip.ac.id>
2024-06-04 02:38:33 +0700

    api test create, get all user done

--- backend/index.js
+++ backend/index.js
@@ -18,16 +18,16 @@ app.use((req,res,next) => {
 });
 
 //  Routes
-app.use(UserRoutes);
+app.use('/api/Users',UserRoutes);
 
-// mongoose.connect('mongodb://localhost:27017/AniCapital_db');
-// const db = mongoose.connection;
-// db.on('error', (error) => console.log(error));
-// db.once('open', () =>console.log('Database Connected...'));
+// connect to db
+mongoose.connect(process.env.MONGO_DB);
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () =>console.log('Database Connected...'));
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(UserRoute);
+
+
 
 // listen request
 app.listen(process.env.PORT, ()=> {
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -8,14 +8,15 @@ const {
 const router = express.Router();
 
 // Get All User
+router.get('/', getUsers);
 
 // Get One User
-
+router.get('/:id', getUserID);
 // Create User
-
+router.post('/', createUser);
 // Delete User
 
-// 
+// Update User
 
 
     
